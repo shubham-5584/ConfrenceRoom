8d77829cc116e0ecc6e879eab76228e96c9f9e3f
Shubham Dhole <dholeshubham666@gmail.com>
2025-02-20 23:26:40 +0530

    Update module-info.java

--- Conference_Room_Booking/src/module-info.java
+++ Conference_Room_Booking/src/module-info.java
@@ -7,3 +7,12 @@
 module Conference_Room_Booking {
 	requires java.sql;
 }
+ System.out.println("\n--- Conference Room Booking System ---");
+                System.out.println("1. Add Conference Room");
+                System.out.println("2. Remove Conference Room");
+                System.out.println("3. Book Conference Room");
+                System.out.println("4. Cancel Booking");
+                System.out.println("5. Print Daily Timetable");
+                System.out.println("6. View Weekly Timetable");
+                System.out.println("7. Exit");
+                System.out.print("Enter your choice: ");
